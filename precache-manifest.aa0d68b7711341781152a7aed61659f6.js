49d01ec9c43a660677c9d6b65cb04978e66a1e55
Nedra Krifa <nedrakrifa@gmail.com>
2020-09-16 00:11:36 +0200

    Updates

---
+++ precache-manifest.aa0d68b7711341781152a7aed61659f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21024a3eef085523106e8c052285c242",
+    "url": "/Redux-Contacts-App/index.html"
+  },
+  {
+    "revision": "1602cd7a994d26c2130c",
+    "url": "/Redux-Contacts-App/static/css/main.5d02160d.chunk.css"
+  },
+  {
+    "revision": "037384986bafc6049581",
+    "url": "/Redux-Contacts-App/static/js/2.9aefdbdc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Redux-Contacts-App/static/js/2.9aefdbdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1602cd7a994d26c2130c",
+    "url": "/Redux-Contacts-App/static/js/main.c6b5643a.chunk.js"
+  },
+  {
+    "revision": "083eff2c3d38c373061c",
+    "url": "/Redux-Contacts-App/static/js/runtime-main.408786ef.js"
+  }
+]);
